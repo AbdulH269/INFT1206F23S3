d12f2c2519cc09e85c998ffa6641e32a6036afe2
AbdulH270 <abdulhadi4@dcmail.ca>
2024-12-02 21:27:33 -0500

    Update a4p2.js
    declared the array of image filenames as well as declaring the alternative texts for each image file

--- assignment4/part2/a4p2.js
+++ assignment4/part2/a4p2.js
@@ -5,8 +5,17 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const alts = {
+    'pic1.jpg' : 'Closeup of a persons eye',
+    'pic2.jpg' : 'wavey rock',
+    'pic3.jpg' : 'Purple and white flowers',
+    'pic4.jpg' : 'Wall from a pharaohs tomb',
+    'pic5.jpg' : 'Moth on a leaf'
+  };
+  
 
 /* Looping through images */
 
